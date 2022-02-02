8ae3cb3366ef0e955668e3610389bc88521dc95e
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-02-01 22:20:50 -0300

    Conceito sobre Reduce em javaScript

---
+++ reduce.js
@@ -0,0 +1,5 @@
+const clientes = [
+  { name: 'Fulano', lastname: 'Da Silva', age: 14 },
+  { name: 'Ciclano', lastname: 'Santos', age: 18 },
+  { name: 'Beltrano', lastname: 'Moreira', age: 20 }
+]
