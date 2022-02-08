13af2d47ad68955992c835a755ba3839859497ed
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-02-07 21:20:59 -0300

    Conceito de asyncAwait javaScript

---
+++ asyncAwait.js
@@ -0,0 +1,30 @@
+/*
+asyncAwait
+*/
+
+function transformarEmJson(response) {
+  //console.log(response)
+  //console.log(response.json)
+  return response.json()
+}
+
+function exibirNaTela (dados) {
+  console.log('exibir na tela', dados)
+}
+
+function exibirErro (dados) {
+  console.log('Ops, deu erro!')
+}
+
+const botaoCarregar = document.querySelector('#botaoCarregar')
+
+const configs = {
+  method: 'GET',
+  header: {}
+}
+
+botaoCarregar.onclick = () =>
+  fetch('https://jsonplaceholder.typicode.com/photos',configs)
+    .then(transformarEmJson)
+    .then(exibirNaTela)
+    .catch(exibirErro)
