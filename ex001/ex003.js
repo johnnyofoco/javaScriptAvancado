3d125e2507c61f7958298d82b5ef41cad1493ded
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-02-11 00:14:27 -0300

    Exercícios javaScript Avançado: ex003

--- ex001/ex003.js
+++ ex001/ex003.js
@@ -1,6 +1,16 @@
-​const aluno = {
-      nome: 'Maria',
-      sobrenome: 'da Silva',
-      notas: [7, 10, 8, 5, 7, 9],
+const aluno = {
+  nome: 'Maria',
+  sobrenome: 'da Silva',
+  notas: [7, 10, 8, 5, 7, 9]
 }
 
+const qtdBimestres = aluno.notas.length
+
+const somaNotas = aluno.notas.reduce((acc, curr) => {
+  acc += curr
+  return acc
+},0)
+
+const media = Math.round(somaNotas / qtdBimestres)
+
+console.log(`A media da ${aluno.nome} ${aluno.sobrenome} foi ${media} para o total de ${qtdBimestres} bimestres.`)
