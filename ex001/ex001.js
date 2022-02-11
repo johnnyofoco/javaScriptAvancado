4b5a3df91cda12f4adea6fc5e623057b0d0de164
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-02-10 23:33:30 -0300

    Exercícios javaScript Avançado: ex001

---
+++ ex001/ex001.js
@@ -0,0 +1,18 @@
+​const alunos = [
+      {
+        nome: 'Maria',
+        sobrenome: 'da Silva',
+      },
+      {
+        nome: 'José',
+        sobrenome: 'Moreira',
+      },
+      {
+        nome: 'Paulo',
+        sobrenome: 'Henrique',
+      },
+      {
+        nome: 'Pedro',
+        sobrenome: 'Souza',
+      },
+]
