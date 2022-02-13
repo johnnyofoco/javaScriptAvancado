2fbbdfd3a7dd7cdc4b4e05f77f03426eb43faf39
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-02-13 12:36:11 -0300

    Exercícios javaScript avançado ex006

---
+++ exercicios/ex006.js
@@ -0,0 +1,20 @@
+const txtMsg = document.querySelector('#txtMsg')
+const btnEnviar = document.querySelector('#btnEnviar')
+const imgLoading = document.querySelector('#imgLoading')
+
+function lerMensagemDigitada () {
+  imgLoading.setAttribute('style', 'display: inline; max-width: 20px;')
+
+  return new Promise((resolve, reject) => {
+    let intervalo = setInterval(() => {
+      imgLoading.setAttribute('style', 'display: none')
+      let msg = txtMsg.value
+      document.write(msg)
+    }, 10000)
+
+    resolve(intervalo)
+    reject()
+  })
+}
+
+btnEnviar.addEventListener('click', lerMensagemDigitada)
